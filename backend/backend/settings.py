3867121773fe48ccd1fceefaa5222dd8247fe609
Rishtha <rishitha.mamidala36@gmail.com>
2024-08-13 17:08:46 -0500

    commit4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = os.environ['SECRET_KEY']
-#SPOONACULAR_API_KEY = os.getenv('SPOONACULAR_API_KEY')
-SPOONACULAR_API_KEY = os.environ['SPOONACULAR_API_KEY']
+SECRET_KEY = os.getenv('SECRET_KEY')
+#SECRET_KEY = os.environ['SECRET_KEY']
+SPOONACULAR_API_KEY = os.getenv('SPOONACULAR_API_KEY')
+#SPOONACULAR_API_KEY = os.environ['SPOONACULAR_API_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
